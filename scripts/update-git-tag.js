491a278f50baf217c174f7cde6d7fd12ae6232dc
Danny Yassine <>
2019-06-27 09:52:37 -0230

    no message

--- scripts/update-git-tag.js
+++ scripts/update-git-tag.js
@@ -9,17 +9,24 @@ const exec = util.promisify(require('child_process').exec);
 
         // await exec('git config --global user.email "${ENV_EMAIL}"', { shell: true});
         // await exec('git config --global user.name "Travis CI"', { shell: true});
-        console.log('1');
-        // const exec1 = await exec(`git tag ${package.version}`, { shell: true});
-        console.log('2');
+
+        console.log(`Running: git tag ${package.version}`);
+        const exec1 = await exec(`git tag ${package.version}`, { shell: true});
+
+        console.log(`Running: git add .`);
         const exec2 = await exec(`git add .`, { shell: true});
-        console.log('3');
+
+        console.log(`Running: git commit -m "Updating version ${package.version}"`);
         const exec3 = await exec(`git commit -m "Updating version ${package.version}"`, { shell: true});
-        console.log('4');
+
+        console.log(`Running: git push && git push --tags`);
         const exec4 = await exec(`git push && git push --tags`, { shell: true});
-        console.log('success');
+
+        console.log('SUCCESS');
         process.exit(0);
     } catch (e) {
+        
+        console.log('ERROR');
         console.log(e);
         process.exit(1);
     }
