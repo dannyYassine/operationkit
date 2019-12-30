ef72557f342878f6a38368a4b8f793adf48f2e36
Danny <dannyyassine@gmail.com>
2019-12-30 12:02:04 -0500

    - types

--- dist/CircularOperationValidator.js
+++ dist/CircularOperationValidator.js
@@ -1,13 +1,12 @@
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class CircularOperationValidatorError extends Error {
     constructor(message) {
         super();
         this.message = message;
     }
 }
-
 class CircularOperationValidator {
-
     /**
      * @param {Array.<Operation>} operations
      */
@@ -15,26 +14,23 @@ class CircularOperationValidator {
         this.operations = operations;
         this._checkCircular();
     }
-
     _checkCircular() {
-        this.operations.forEach(operation => {
+        this.operations.forEach((operation) => {
             operation.dependencies.forEach(op => {
                 let map = {};
                 map[operation.id] = Object.keys(map).length;
-                this._verifyOpMap(op, map);                
-                this._checkDependencies(op, map)
+                this._verifyOpMap(op, map);
+                this._checkDependencies(op, map);
             });
         });
     }
-
     _checkDependencies(operation, mapHash) {
-        operation.dependencies.forEach(op => {
+        operation.dependencies.forEach((op) => {
             let map = JSON.parse(JSON.stringify(mapHash));
             this._verifyOpMap(op, map);
-            this._checkDependencies(op, map)
-        })
+            this._checkDependencies(op, map);
+        });
     }
-
     /**
      * @param {Operation} op
      * @param map
@@ -42,28 +38,20 @@ class CircularOperationValidator {
      */
     _verifyOpMap(op, map) {
         if (map[op.id] !== undefined) {
-            this._throwError(op, map)
+            this._throwError(op, map);
         }
-
         map[op.id] = Object.keys(map).length;
     }
-
     _throwError(op, map) {
-        let keys = Object.keys(map);
-
-            let values = {};
-            for (let key in map) {
-                const value = map[key];
-                values[value] = key;
-            }
-
-            values = Object.values(values);
-            values.push(op.id);
-            throw new CircularOperationValidatorError(`Circular: ${values}`);
+        let mapValues = {};
+        for (let key in map) {
+            const value = map[key];
+            mapValues[value] = key;
+        }
+        const values = Object.values(mapValues);
+        values.push(op.id);
+        throw new CircularOperationValidatorError(`Circular: ${values}`);
     }
-
 }
-
-module.exports = {
-    CircularOperationValidator
-};
+exports.CircularOperationValidator = CircularOperationValidator;
+//# sourceMappingURL=CircularOperationValidator.js.map
